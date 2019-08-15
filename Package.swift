2b4f05cbdf7a88f790419ca65ba3d83de28d9559
NianJi <765409243@qq.com>
2019-08-15 16:12:52 +0800

    update

--- Package.swift
+++ Package.swift
@@ -8,8 +8,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
+            name: "cocore",
+            targets: ["cocore"]),
+        .library(
             name: "coswift",
-            targets: ["coswift", "cocore"]),
+            targets: ["coswift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
