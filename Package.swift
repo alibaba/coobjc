33f93a7b47fef8fa484a7f196166f8544631413b
NianJi <765409243@qq.com>
2019-08-15 15:28:02 +0800

    更新package

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "cocore",
+            path: "cocore",
             dependencies: []),
         .target(
             name: "coswift",
+            path: "coswift",
             dependencies: ["cocore"]),
         .testTarget(
             name: "coswiftTests",
